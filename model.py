aed826becdcb15e1f405f0809a7cf12a89a7a35b
Ubuntu <ubuntu@ip-172-31-9-130.eu-north-1.compute.internal>
2026-06-16 13:46:11 +0000

    Deploy TB inference pipeline on AWS with Docker

--- model.py
+++ model.py
@@ -1,20 +1,24 @@
-# app/model.py
 
 import torch
 from timm import create_model
+
 
 def load_model():
     device = torch.device("cpu")  # keep CPU for now
 
     model = create_model(
-        'vit_base_patch16_224',
+        "vit_base_patch16_224",
         pretrained=False,
         num_classes=2
     )
 
     model.load_state_dict(
-        torch.load("vit_model.pth", map_location=device)
+        torch.load(
+            "models/vit_model.pth",
+            map_location=device
+        )
     )
 
     model.eval()
     return model
+
